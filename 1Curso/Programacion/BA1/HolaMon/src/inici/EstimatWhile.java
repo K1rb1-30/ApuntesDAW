22a107a8e1a9832d0033a172bbf27efbd36dfddc
Gabriel Sánchez Flores <f1resecundario@gmail.com>
2025-09-30 17:58:46 +0200

    a

---
+++ 1Curso/Programacion/BA1/HolaMon/src/inici/EstimatWhile.java
@@ -0,0 +1,17 @@
+package inici;
+
+public class EstimatWhile {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		int a = 5;
+		while(a < 10) {
+			System.out.println("La variable a val: " + a);
+			a ++;
+		}
+
+	}
+
+}
---
+++ 1Curso/Programacion/BA1/HolaMon/src/joel/HoraValida.java
@@ -0,0 +1,10 @@
+package joel;
+
+public class HoraValida {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
