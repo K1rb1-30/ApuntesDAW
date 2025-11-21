e7a4583551348d66298fe85cb19c18477d9b6712
Gabriel Sánchez Flores <f1resecundario@gmail.com>
2025-11-21 18:37:27 +0100

    ejercicios joel + miniprovaarrays

--- 1Curso/Programacion/BA1/HolaMon/src/arrayssplit/Contrasenyes.java
+++ 1Curso/Programacion/BA1/HolaMon/src/arrayssplit/Contrasenyes.java
@@ -1,5 +1,6 @@
 package arrayssplit;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Contrasenyes {
@@ -15,9 +16,13 @@ public class Contrasenyes {
 		
 		for(int i = 0; i < vectorT; i++) {
 			listapalabras[i] = sc.next();
-			listacontrasenyas[i] = sc.next();
 			
 		}
+		
+		for(int r = 0; r < vectorT; r++) {
+			listacontrasenyas[r] = sc.next();
+		}
+		
 		
 		int vectorN = sc.nextInt();
 		
@@ -25,8 +30,36 @@ public class Contrasenyes {
 			String comprouser = sc.next();
 			String comprocontrase = sc.next();
 			
+			boolean userencontrado = false;
+			boolean pswdencontrado = false;
 			
+			for(int q = 0; q < listapalabras.length; q++) {
+				if(comprouser.equals(listapalabras[q])) {
+					userencontrado = true;
+				}
+			}
+			
+			for(int t = 0; t < listapalabras.length; t++) {
+				if(comprocontrase.equals(listacontrasenyas[t])) {
+					pswdencontrado = true;
+				}
+			}
+			
+			if(!userencontrado) {
+				System.out.println("usuari no trobat");
+			}else if (userencontrado && !pswdencontrado) {
+				System.out.println("contrasenya incorrecte");
+			}else if (userencontrado && pswdencontrado) {
+				System.out.println("OK");
+			}
 		}
+		
+		Arrays.sort(listapalabras);
+		
+		for(int u = 0; u < listapalabras.length; u++) {
+			System.out.print(listapalabras[u] + " ");
+		}
+		
 		
 	}
 
---
+++ 1Curso/Programacion/BA1/HolaMon/src/arrayssplit/Rimador.java
@@ -0,0 +1,25 @@
+package arrayssplit;
+
+import java.util.Scanner;
+
+public class Rimador {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int casos = sc.nextInt();
+		
+		for(int i = 0; i < casos; i++) {
+			String frase = sc.nextLine();
+			
+			String[] rima = frase.split(", ");
+			
+			
+			
+		}
+
+	}
+
+}
