10c9923e6a0670ff80081274a60016a3829e51c0
K1rb1-30 <f1resecundario@gmail.com>
2026-01-27 17:58:22 +0100

    memory update

--- 1Curso/Programacion/BA1/MemoryGabrielSanchezFlores/src/joc/Memory.java
+++ 1Curso/Programacion/BA1/MemoryGabrielSanchezFlores/src/joc/Memory.java
@@ -1,6 +1,8 @@
 package joc;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Random;
 import java.util.Scanner;
 
 public class Memory {
@@ -71,23 +73,166 @@ public class Memory {
 
 	private static void Juego(Jugador j1, Jugador j2) {
 		// TODO Auto-generated method stub
-		String[][] tableroMuestra = inicializarTablero();
-		String[][] tableroReal = inicializarTableroReal();
+		
+		String[][] tableroMuestra = inicializarTablero(); //Primero inicializamos el tablero que se mostrara
+		String[][] tableroReal = inicializarTableroReal(); // Luego iniciamos el tablero real que sera con el que haremos las comprobaciones
+		
+		boolean ganador = false; //Esto es para que se repita el bucle de juego hasta que alguien gane
+		String nombreganador = " ";
+		Random r = new Random();
+		int turno = r.nextInt(1,3); //El juego elige aleatoriamente quien empieza siendo 1 el j1 y 2 el j2
+		
+		while(!ganador) {
+			printarTablero(tableroMuestra);
+			
+			if(turno == 1) {
+				System.out.println("Turno del jugador 1 " + j1.nom);
+				boolean acierto = seleccionDeFitxas(tableroMuestra, tableroReal);
+				if(acierto) {
+					ganador = comprovarGanador(tableroMuestra, tableroReal);
+					if(ganador == true) {
+						nombreganador = j1.nom;
+					}
+				}else {
+					turno = 2;
+				}
+				
+				
+			}else if(turno == 2) {
+				System.out.println("Turno del jugador 2 " + j2.nom);
+				boolean acierto = seleccionDeFitxas(tableroMuestra, tableroReal);
+				if(acierto) {
+					ganador = comprovarGanador(tableroMuestra, tableroReal);
+					if(ganador == true) {
+						nombreganador = j2.nom;
+					}
+				}else {
+					turno = 1;
+				}
+			}
+		}
+		
+		
+		
 		
 		
 		
 	}
 
+	private static boolean comprovarGanador(String[][] tableroMuestra, String[][] tableroReal) {
+		// TODO Auto-generated method stub
+		if(tableroMuestra.equals(tableroReal)) {
+			return true;
+		}else {
+			return false;
+		}
+		
+	}
+
+	private static boolean seleccionDeFitxas(String[][] tableroMuestra, String[][] tableroReal) {
+		// TODO Auto-generated method stub
+		
+		int[] primeraficha = pedirPosicion(tableroMuestra, tableroReal);
+		
+		printarTablero(tableroMuestra);
+				
+		System.out.println();
+		
+		int[] segundaficha = pedirPosicion(tableroMuestra, tableroReal);
+		
+		printarTablero(tableroMuestra);
+		
+		boolean acierto = compararIgualdad(primeraficha, segundaficha, tableroMuestra, tableroReal);
+		
+		return acierto;
+	}
+
+	private static boolean compararIgualdad(int[] primeraficha, int[] segundaficha, String[][] tableroMuestra,
+			String[][] tableroReal) {
+		// TODO Auto-generated method stub
+		
+		if(tableroReal[primeraficha[0]][primeraficha[1]].equals(tableroReal[segundaficha[0]][segundaficha[1]])) {
+			System.out.println("Has acertado");
+			return true;
+		}else {
+			System.out.println("Has Fallado");
+			tableroMuestra[primeraficha[0]][primeraficha[1]] = "X";
+			tableroMuestra[segundaficha[0]][segundaficha[1]] = "X";
+			return false;
+		}
+		
+	}
+
+	private static int[] pedirPosicion(String[][] tableroMuestra, String[][] tableroReal) {
+		// TODO Auto-generated method stub
+		boolean correcto1 = false; //esto hara de que si introduces algo incorrecto como una ficha que ya esta destapada o un valor fuera de la matriz te vuelva a pedir
+		
+		int fila1 = 0;
+		int columna1 = 0;
+		
+		int[]posiciones = new int[2];
+				
+		while(!correcto1) {
+			System.out.println("Selecciona una ficha por fila(x) y columna(y) (ej. 3 0): ");
+			fila1 = sc.nextInt();
+			columna1 = sc.nextInt();
+			
+			if(noEsticFora(tableroReal, fila1, columna1) && tableroMuestra[fila1][columna1].equals("X") ) {
+				tableroMuestra[fila1][columna1] = tableroReal[fila1][columna1];
+				correcto1 = true;
+			}else {
+				System.out.println("ERROR al indicar la ficha recuerda que es del 0 - " + (tableroMuestra.length - 1) + " y primero van las filas y luego las columnas (ej 0 "+ (tableroMuestra.length - 2) + ")." );
+			}
+		}
+		
+		posiciones[0] = fila1;
+		posiciones[1] = columna1;
+		
+		return posiciones;
+	}
+
+	private static boolean noEsticFora(String[][] mat2, int f, int c) {
+
+		if (f < 0 || c < 0 || f > mat2.length - 1 || c > mat2[0].length - 1) {
+			return false;
+		} else {
+
+			return true;
+		}
+	}
+
 	private static String[][] inicializarTableroReal() {
 		// TODO Auto-generated method stub
-		int numParelles = (sizetableroX + sizetableroY)/2;
+		int numParelles = (sizetableroX * sizetableroY)/2;
 		
-		ArrayList<String> fitxesA = new ArrayList<String>();
-		ArrayList<String> fitxesB = new ArrayList<String>();
+		String[][] tableroReal = new String[sizetableroX][sizetableroY];
 		
+		ArrayList<String> fitxes = new ArrayList<String>();
 		
+		for(int i = 1;  i <= numParelles; i++) {
+			fitxes.add(Integer.toString(i));
+		}
 		
-		return null;
+		for(int i = 1;  i <= numParelles; i++) {
+			fitxes.add(Integer.toString(i));
+		}
+		
+//		System.out.println(fitxes);
+		Collections.shuffle(fitxes);
+//		System.out.println(fitxes);
+		
+		int contador = 0;
+		
+		for (int i = 0; i < tableroReal.length; i++) {
+			for (int j = 0; j < tableroReal[0].length; j++) {
+				tableroReal[i][j] = fitxes.get(contador);
+				contador++;
+			}
+		}
+		
+		printarTablero(tableroReal); // Quitar esto depues
+		
+		return tableroReal;
 	}
 
 	private static String[][] inicializarTablero() {
@@ -104,7 +249,7 @@ public class Memory {
 	private static void printarTablero(String[][] mat) {
 		for (int i = 0; i < mat.length; i++) {
 			for (int j = 0; j < mat[0].length; j++) {
-				System.out.print(mat[i][j] + " ");
+				System.out.print(" " + mat[i][j] + " ");
 			}
 			System.out.println();
 		}
