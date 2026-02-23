ca6966a169bc548538ea392f3f18817f6ef97160
K1rb1-30 <f1resecundario@gmail.com>
2026-02-23 17:02:36 +0100

    Recursividad empezada

---
+++ 1Curso/Programacion/BA1/HolaMon/src/recursividad/Bitlles1.java
@@ -0,0 +1,35 @@
+package recursividad;
+
+import java.util.Scanner;
+
+public class Bitlles1 {
+
+static Scanner sc = new Scanner(System.in);
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int casos = sc.nextInt();
+		
+		for (int i = 0; i < casos; i++) {
+			
+			int bombons = 0;
+			int pisos = sc.nextInt();
+			
+			bombons = piramide(pisos, bombons);
+			System.out.println(bombons);
+			
+		
+		}
+	}
+
+	private static int piramide(int pisos, int bombons) {
+		// TODO Auto-generated method stub
+		if(pisos == 0) {
+			return bombons;
+		}else {
+			return(piramide(pisos-1, bombons + pisos));
+		}
+	}
+
+}
---
+++ 1Curso/Programacion/BA1/HolaMon/src/recursividad/CalcularFactorial.java
@@ -0,0 +1,39 @@
+package recursividad;
+
+import java.util.Scanner;
+
+public class CalcularFactorial {
+	
+	static Scanner sc = new Scanner(System.in);
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int casos = sc.nextInt();
+		
+		for (int i = 0; i < casos; i++) {
+			
+			long factorial = 0;
+			int num = sc.nextInt();
+			
+			factorial = Calcularfactorial(num);
+			
+		
+		}
+	}
+
+	private static long Calcularfactorial(int num) {
+		// TODO Auto-generated method stub
+		
+		
+		
+		if(num < 2)
+	}
+
+	
+
+
+
+
+}
+
+
