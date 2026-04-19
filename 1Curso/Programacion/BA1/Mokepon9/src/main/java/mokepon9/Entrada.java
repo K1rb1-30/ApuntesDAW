c50472716e57e12b5e3caee7bbe35d024466663a
K1rb1-30 <f1resecundario@gmail.com>
2026-04-19 17:54:06 +0200

    mokepon9

--- 1Curso/Programacion/BA1/Mokepon9/src/main/java/mokepon9/Entrada.java
+++ 1Curso/Programacion/BA1/Mokepon9/src/main/java/mokepon9/Entrada.java
@@ -1,20 +1,25 @@
 package mokepon9;
 
+import java.util.ArrayList;
+
 import javax.xml.bind.annotation.XmlAttribute;
 import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlElementWrapper;
 import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.XmlType;
 
 //l'element es diu persona en el XML
 @XmlRootElement (name = "entrada")
 //l'ordre en que apareixeran els seus elements interns en el XML
-@XmlType(propOrder = {"nom","tipus"})
+@XmlType(propOrder = {"nom","tipus","pes","evolucions","localitzacions"})
 
 public class Entrada {
   
   int id;
   String nom, tipus;
-
+  double pes;
+  Evolucions evolucions;
+  ArrayList<Localitzacio> localitzacions = new ArrayList<>();
 
   public Entrada() {
 	super();
@@ -25,6 +30,27 @@ public class Entrada {
 	this.id = id;
 	this.nom = nom;
 	this.tipus = tipus;
+}
+  
+
+
+  public Entrada(int id, String nom, String tipus, double pes, Evolucions evolucions) {
+	super();
+	this.id = id;
+	this.nom = nom;
+	this.tipus = tipus;
+	this.pes = pes;
+	this.evolucions = evolucions;
+}
+  public Entrada(int id, String nom, String tipus, double pes, Evolucions evolucions,
+		ArrayList<Localitzacio> localitzacions) {
+	super();
+	this.id = id;
+	this.nom = nom;
+	this.tipus = tipus;
+	this.pes = pes;
+	this.evolucions = evolucions;
+	this.localitzacions = localitzacions;
 }
   //es un atribut. els atributs son els que es posen a la propia etiqueta arrel
   @XmlAttribute
@@ -39,9 +65,6 @@ public class Entrada {
   public String getNom() {
       return nom;
   }
-  
-
-
   public void setNom(String nom) {
       this.nom = nom;
   }
@@ -53,10 +76,38 @@ public class Entrada {
       return tipus;
   }
   
-
-
   public void setTipus(String t) {
       this.tipus = t;
   }
+  
+  @XmlElement
+  public double getPes() {
+	return pes;
+  }
+  public void setPes(double pes) {
+	this.pes = pes;
+  }
+  
+  @XmlElement
+  public Evolucions getEvolucions() {
+	return evolucions;
+  }
+  public void setEvolucions(Evolucions evolucions) {
+	this.evolucions = evolucions;
+  }
+  
+  @XmlElementWrapper(name = "localitzacions")
+  @XmlElement(name = "localitzacio")
+  public ArrayList<Localitzacio> getLocalitzacions() {
+	return localitzacions;
+  }
+  public void setLocalitzacions(ArrayList<Localitzacio> localitzacions) {
+	this.localitzacions = localitzacions;
+  }
+  
+  
+  
+  
+  
 }
 
--- 1Curso/Programacion/BA1/Mokepon9/src/main/java/mokepon9/Test.java
+++ 1Curso/Programacion/BA1/Mokepon9/src/main/java/mokepon9/Test.java
@@ -11,14 +11,36 @@ public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Entrada pikachu = new Entrada(25, "Mikachu", "Electric");
-		Entrada piplup = new Entrada(393, "Piplup", "Aigua");
-		EscriureEntrada(piplup);
-		Mokedex pokedex = new Mokedex("Gabri");
-		pokedex.entrades.add(piplup);
-		pokedex.entrades.add(pikachu);
-		EscriureMokedex(pokedex);
+//		Evolucions pikaevo = new Evolucions("Pichu", "Raichu");
+//		Localitzacio paleta = new Localitzacio("Pueblo Paleta");
+//		Entrada pikachu = new Entrada(25, "Pikachu", "Electric", 6.0, pikaevo);
+//		pikachu.localitzacions.add(paleta);
+//		
+//		Evolucions pipluaevo = new Evolucions("-", "Prinplup");
+//		Entrada piplup = new Entrada(393, "Piplup", "Aigua", 5.2, pipluaevo);
+//		Localitzacio veraz = new Localitzacio("Lago Veraz");
+//		piplup.localitzacions.add(paleta);
+//		piplup.localitzacions.add(veraz);
+//		EscriureEntrada(piplup);
+//		Mokedex pokedex = new Mokedex("Gabri");
+//		pokedex.entrades.add(piplup);
+//		pokedex.entrades.add(pikachu);
+//		EscriureMokedex(pokedex);
 		
+//		Evolucions empoevo = new Evolucions("Prinplup", "-");
+//		Entrada empoleon = new Entrada(395, "Empoleon", "Aigua", 84.5, empoevo);
+//		
+//		afegirMokedex("mokedex.xml", empoleon);
+		
+//		modificarPes("mokedex.xml", 395, 10);
+//		modificarPes("mokedex.xml", 100, 1);
+		
+//		afegirLocalitzacio("mokedex.xml", 395, "Ciudad Plateada");
+//		afegirLocalitzacio("mokedex.xml", 10, "Cueva del Team Rocket");
+		
+		
+//		postEvolucio("mokedex.xml", 395);
+//		postEvolucio("mokedex.xml", 393);
 		
 		
 		
@@ -93,5 +115,129 @@ public class Test {
 		}
 
 	}
+	
+	public static void afegirMokedex(String nomFitxer, Entrada novaEntrada) {
+		File fitxerDesti = new File(nomFitxer);
+		try {
+		    JAXBContext jaxbContext = JAXBContext.newInstance(Mokedex.class);
+		    Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
+		   
+		    Mokedex mokedex = (Mokedex) jaxbUnmarshaller.unmarshal(fitxerDesti);
+		    
+		    mokedex.getEntrades().add(novaEntrada);
+		    
+		    Marshaller jaxbMarshaller = jaxbContext.createMarshaller();
+		    jaxbMarshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
+		    jaxbMarshaller.marshal(mokedex, fitxerDesti);
+		    
+		} catch (JAXBException je) {
+		    je.printStackTrace();
+		}
+	}
+	
+	public static void modificarPes(String nomFitxer, int idEntrada, double pesExtra) {
+		File fitxerDesti = new File(nomFitxer);
+		try {
+		    JAXBContext jaxbContext = JAXBContext.newInstance(Mokedex.class);
+		    Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
+		   
+		    Mokedex mokedex = (Mokedex) jaxbUnmarshaller.unmarshal(fitxerDesti);
+		    
+		    boolean encontrado = false;
+		    
+		    for(Entrada e : mokedex.getEntrades()) {
+		    	if(e.getId() == idEntrada) {
+		    		e.setPes(pesExtra + e.getPes());
+		    		encontrado = true;
+		    	}
+		    }
+		    
+		    if(!encontrado) {
+		    	System.out.println("Error aquesta entrada no existeix");
+		    	return;
+		    }
+		    
+		    Marshaller jaxbMarshaller = jaxbContext.createMarshaller();
+		    jaxbMarshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
+		    jaxbMarshaller.marshal(mokedex, fitxerDesti);
+		    
+		    System.out.println("Pes modificat");
+		    
+		} catch (JAXBException je) {
+		    je.printStackTrace();
+		}
+	}
+	
+	public static void afegirLocalitzacio(String nomFitxer, int idEntrada, String novaLoc) {
+		File fitxerDesti = new File(nomFitxer);
+		try {
+		    JAXBContext jaxbContext = JAXBContext.newInstance(Mokedex.class);
+		    Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
+		   
+		    Mokedex mokedex = (Mokedex) jaxbUnmarshaller.unmarshal(fitxerDesti);
+		    
+		    boolean encontrado = false;
+		    
+		    for(Entrada e : mokedex.getEntrades()) {
+		    	if(e.getId() == idEntrada) {
+		    		Localitzacio l = new Localitzacio(novaLoc);
+		    		e.getLocalitzacions().add(l);
+		    		encontrado = true;
+		    	}
+		    }
+		    
+		    if(!encontrado) {
+		    	System.out.println("Error aquesta entrada no existeix");
+		    	return;
+		    }
+		    
+		    Marshaller jaxbMarshaller = jaxbContext.createMarshaller();
+		    jaxbMarshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
+		    jaxbMarshaller.marshal(mokedex, fitxerDesti);
+		    
+		    System.out.println("Localitzacio Afegida");
+		    
+		} catch (JAXBException je) {
+		    je.printStackTrace();
+		}
+	}
+	
+	public static void postEvolucio(String nomFitxer, int idEntrada) {
+		File fitxerDesti = new File(nomFitxer);
+		try {
+		    JAXBContext jaxbContext = JAXBContext.newInstance(Mokedex.class);
+		    Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
+		   
+		    Mokedex mokedex = (Mokedex) jaxbUnmarshaller.unmarshal(fitxerDesti);
+		    
+		    boolean encontrado = false;
+		    
+		    for(Entrada e : mokedex.getEntrades()) {
+		    	if(e.getId() == idEntrada) {
+		    		String poste = e.getEvolucions().getPost_evolucio();
+		    		if(poste.equals("-")) {
+		    			System.out.println( e.getNom() + " no te post evolució.");
+		    		}else {
+		    			System.out.println("La post evolució de " + e.getNom() + " es " + poste + ".");
+		    		}
+		    		encontrado = true;
+		    	}
+		    }
+		    
+		    if(!encontrado) {
+		    	System.out.println("Error aquesta entrada no existeix");
+		    	return;
+		    }
+		    
+		    Marshaller jaxbMarshaller = jaxbContext.createMarshaller();
+		    jaxbMarshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
+		    jaxbMarshaller.marshal(mokedex, fitxerDesti);
+		    
+		} catch (JAXBException je) {
+		    je.printStackTrace();
+		}
+	}
+	
+	
 	
 }
