48e14ce63cdb4fee8616878dc030859b2a78e3fa
K1rb1-30 <f1resecundario@gmail.com>
2026-05-18 20:28:49 +0100

    AP5 php

---
+++ 1Curso/HTML/BA3/AP5/ex1/databaseHecho.php
@@ -0,0 +1,14 @@
+<?php
+
+DROP DATABASE IF EXISTS ap5php;
+
+CREATE DATABASE ap5php CHARACTER SET utf8;
+
+USE ap5php;
+
+CREATE TABLE usuari(
+    idUsuari int AUTO_INCREMENT,
+	email	VARCHAR(40),
+	contrasenya VARCHAR(50),
+	CONSTRAINT pk_usuari PRIMARY KEY (idUsuari)
+);
