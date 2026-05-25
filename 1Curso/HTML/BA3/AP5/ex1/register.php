f9610c754f461e49f2a4f57408773421fcd223a1
K1rb1-30 <f1resecundario@gmail.com>
2026-05-25 15:37:23 +0100

    Update register.php

--- 1Curso/HTML/BA3/AP5/ex1/register.php
+++ 1Curso/HTML/BA3/AP5/ex1/register.php
@@ -10,14 +10,32 @@ try {
     $email = $_POST["email"];
     $pswd = $_POST["pswd"];
 
-
     try {
-        $query = $conn->prepare("insert into usuari values(:email, :pswd)");
+        $query = $conn->prepare("select * from usuari where  :email OR :pswd)");
         $query->bindParam(":email", $email);
         $query->bindParam(":pswd", $pswd);
         $query->execute();
-        $result = $query->fetchAll(PDO::FETCH_ASSOC);
-        print_r(json_encode($result));
+
+        // si devuelve la consulta es que existe
+        if ($query->rowCount() < 0) {
+            $resposta = [
+                "estatus" => "OK",
+                "error"=> "",
+                "usuari_app" => $email
+            ];
+
+            print_r(json_encode($resposta));
+
+        }else{
+            $resposta = [
+                "estatus" => "KO",
+                "missatge" => "usuari o contrasenya incorrecte"
+            ];
+            print_r(json_encode($resposta));
+        }
+
+        #$result = $query->fetchAll(PDO::FETCH_ASSOC);
+        #print_r(json_encode($result));
 
     } catch (Exception $exception) {
         print_r($exception->getMessage());
